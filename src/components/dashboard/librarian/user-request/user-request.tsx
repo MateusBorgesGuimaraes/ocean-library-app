99a20c023676473723115fda5d345c726fc37e72
Mateus Borges <mateusguimaraes717@gmail.com>
2024-12-25 10:12:19 -0300

    request paginacao

--- src/components/dashboard/librarian/user-request/user-request.tsx
+++ src/components/dashboard/librarian/user-request/user-request.tsx
@@ -1,47 +1,104 @@
+'use client';
+
 import { GeneralInfosCard } from '@/components/general-infos-card/general-infos-card'
 import styles from './user-request.module.css'
 import { TitleHeader } from "@/components/title-header/title-header"
 import { Button } from '@/components/button/button'
 import Image from 'next/image'
 import { icons } from '../../../../../public/assets/assets'
+import React from 'react'
+import { requestService } from '@/services/api/request-service'
+import { RequestBook } from '@/services/api/types/request-types';
+import { usePagination } from '@/hooks/useFetch';
+import { PaginationControls } from '@/components/pagination-controls/pagination-controls';
+import formatDate from '@/functions/fomatDate';
+import { Loader } from '@/components/loader/loader';
+import { useToastStore } from '@/store/toast-store';
+import { ApiError } from 'next/dist/server/api-utils';
 
 export const UserRequest = () => {
+  const [requestData, setRequestData] = React.useState< RequestBook[]>([]);
+  const addToast = useToastStore((state) => state.addToast);
+  const {
+      data: requests,
+      meta,
+      loading,
+      error,
+      nextPage,
+      prevPage,
+    } = usePagination<RequestBook>({
+      fetchFn: requestService.getAllRequests,
+      initialParams: void 0,
+      initialPage: 1,
+      initialLimit: 6,
+      skip: false,
+    });
+
+  React.useEffect(() => {    
+      setRequestData(requests);
+  }, [requests]);
+
+  const removeRequest = async (id: string) => {
+    try {
+      await requestService.removeRequest(id);
+      addToast({  
+        title: 'Success',
+        type: 'success',
+        message: 'Request removed successfully',
+        duration: 5000,
+      });
+
+      setRequestData((prev) => prev.filter((item) => item.id !== +id));
+    } catch (error) {
+      if (error instanceof ApiError) {
+        addToast({
+          title: 'Error',
+          type: 'error',
+          message: error.message,
+          duration: 5000,
+        });
+      } else {
+        addToast({
+          title: 'Error',
+          type: 'error',
+          message: 'An unexpected error occurred',
+          duration: 5000,
+        });
+      }
+    }
+  }
+
   return (
     <div className={styles.userRequestContainer}>
       <TitleHeader title="Users Requests" />
       <div className={styles.cardContainer}>
-        <GeneralInfosCard>
-          <GeneralInfosCard.Content> 
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-          </GeneralInfosCard.Content>
-          <GeneralInfosCard.Footer>
-            <Button fontSize='1rem' background='#055A8C' color='#fff' padding='.25rem 1.125rem'>viewed <Image src={icons.eyeViewIcon} alt="eye view icon" /></Button>
-          </GeneralInfosCard.Footer>
-        </GeneralInfosCard>
+        {loading && <Loader />}
 
-        <GeneralInfosCard>
-          <GeneralInfosCard.Content> 
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-          </GeneralInfosCard.Content>
-          <GeneralInfosCard.Footer>
-            <Button fontSize='1rem' background='#055A8C' color='#fff' padding='.25rem 1.125rem'>viewed <Image src={icons.eyeViewIcon} alt="eye view icon" /></Button>
-          </GeneralInfosCard.Footer>
-        </GeneralInfosCard>
-
-        <GeneralInfosCard>
-          <GeneralInfosCard.Content> 
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-            <GeneralInfosCard.Content.ContentItem label="Name" content="Jhon Doe" />
-          </GeneralInfosCard.Content>
-          <GeneralInfosCard.Footer>
-            <Button fontSize='1rem' background='#055A8C' color='#fff' padding='.25rem 1.125rem'>viewed <Image src={icons.eyeViewIcon} alt="eye view icon" /></Button>
-          </GeneralInfosCard.Footer>
-        </GeneralInfosCard>
+        { requests && !loading &&
+          requestData.map((item) => (
+            <GeneralInfosCard key={item.id}>
+              <GeneralInfosCard.Content> 
+                <GeneralInfosCard.Content.ContentItem label="Title" content={item.title} />
+                <GeneralInfosCard.Content.ContentItem label="Author" content={item.author} />
+                <GeneralInfosCard.Content.ContentItem label="createdAt" content={formatDate(item.createdAt)} />
+                <GeneralInfosCard.Content.ContentItem label="publisher" content={item.publisher} />
+                <GeneralInfosCard.Content.ContentItem label="year" content={item.year} />
+                <GeneralInfosCard.Content.ContentItem label="genre" content={item.genre} />
+              </GeneralInfosCard.Content>
+              <GeneralInfosCard.Footer>
+                <Button onClick={() => removeRequest(String(item.id))} fontSize='1rem' background='#055A8C' color='#fff' padding='.25rem 1.125rem'>viewed <Image src={icons.eyeViewIcon} alt="eye view icon" /></Button>
+              </GeneralInfosCard.Footer>
+            </GeneralInfosCard>
+          ))
+        }
+      </div>
+      <div>
+        <PaginationControls
+          prevPage={prevPage}
+          nextPage={nextPage}
+          page={meta.page}
+          totalPages={meta.totalPages}
+        />
       </div>
     </div>
   )
--- src/services/api/request-service.ts
+++ src/services/api/request-service.ts
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { apiClient } from './base-api-client';
 import { CreatedRequest, RequestBook } from './types/request-types';
+import { ApiError } from './utils/api-error';
 
 export const requestService = {
   async postRequest(requestData: CreatedRequest): Promise<RequestBook> {
@@ -12,5 +14,29 @@ export const requestService = {
     });
 
     return response.data;
+  },
+
+  async getAllRequests(params: void, page: number = 1, limit: number = 6) : Promise<{ data: RequestBook[]; meta: { total: number; page: number; totalPages: number }; }> {
+    const response = await apiClient.get(
+      `/requests?page=${page}&limit=${limit}`,
+    );
+    return {
+      data: response.data.data,
+      meta: {
+        total: response.data.meta.totalItems,
+        page: response.data.meta.currentPage,
+        totalPages: response.data.meta.totalPages,
+      },
+    };
+  },
+
+  async removeRequest(id: string): Promise<void> {
+    try {
+      await apiClient.delete(`/requests/${id}`);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw new ApiError(error);
+      }
+    }
   },
 };
